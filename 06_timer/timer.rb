803db7b81e564ba9ffc8b76306d9e2c461d51769
Omar Diaz <DiazOmarIsaac@gmail.com>
2019-06-12 17:50:59 -0500

    Answered 06_timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,17 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  def initialize()
+    @seconds = 0
+  end
+  
+  def time_string
+    hours = @seconds / 3600
+    minutes = @seconds / 60
+    minutes = minutes - (hours.floor * 60)
+    @seconds = @seconds - ( (hours.floor * 3600) + (minutes.floor * 60) )
+    #@seconds.to_s.length > 1? @seconds = @seconds.to_s : @seconds = "0#{@seconds}"
+    #time_string = "0#{hours.floor}:0#{minutes.floor}:#{seconds}"
+    time_string = "%02d:%02d:%02d" % [hours,minutes,seconds]
+  end
+
 end
