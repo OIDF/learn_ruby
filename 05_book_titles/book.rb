f4bea1cfaed7bb3869031c7f8d79382a90fcb396
Omar Diaz <DiazOmarIsaac@gmail.com>
2019-06-12 16:54:22 -0500

    Answered 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+    
+   def title
+        @title
+   end
+
+    def title=(str)
+       new_title = str.split.map do |word|  
+            if %w(the and an a in of).include?(word)
+                    word
+            else
+                word.capitalize
+            end
+        end
+        new_title.first.capitalize!
+       @title = new_title.join(" ")
+    end
+
 end
